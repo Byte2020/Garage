7a022cde4e71f684d7707b51fd20f708bcedbf46
Zezen22 <superpeio@yahoo.com>
2018-02-28 16:04:43 +0100

    I created a Garage Class with the methods the Console needs to be implemented.

---
+++ src/Garage.java
@@ -0,0 +1,37 @@
+
+
+
+
+public class Garage {
+	private String name;
+	private int capacity;
+	public Garage(String name, int capacity) {
+		this.name = name;
+		this.capacity = capacity;
+	}
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	public void checkIn(Car vehicule) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Car findVehicule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void checkOut(Car vehicule) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void printVehiculesInGarage() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+
+}
