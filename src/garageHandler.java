39cdcf57b770c629216e14d48fd6c1941514742e
Miguel Chourio <Byte2020>
2018-03-02 09:53:50 +0100

    Garage handler & Garage

--- src/garageHandler.java
+++ src/garageHandler.java
@@ -2,39 +2,35 @@ import utilities.VehicleType;
 
 public class garageHandler {
 
-	Garage garage=new Garage();
-	
+	Garage garage = new Garage();
+
 	public garageHandler() {
-		
-		
+
 	}
-	
-	
+
 	public void listParkedVehicles() {
-		
-		
-	}garage.printOutVehicleDetails();
-	
+
+		garage.printOutVehicleDetails();
+	}
+
 	public void listVehicleTypes() {
-		//TODO
+		// TODO
 	}
 
 	public void checkIn(Vehicle visitor) {
-		if(visitor.getType()==VehicleType.CAR ||visitor.getType()==VehicleType.MOTORBIKE )
-		garage.addVehicles(visitor);
-		
+		if (visitor.getType() == VehicleType.CAR || visitor.getType() == VehicleType.MOTORBIKE)
+			garage.addVehicles(visitor);
+
 	}
 
 	public void checkOut(String regPlate) {
-		Vehicle vehicle=garage.findPlate();
-		
+		Vehicle vehicle = garage.findPlate();
+
 	}
 
 	public void registerParkedVehicles() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	
 }
