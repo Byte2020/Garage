953a8111a090a3bed02ab5774a0c964049783543
Miguel Chourio <Byte2020>
2018-03-02 12:01:41 +0100

    Garage

--- src/Garage.java
+++ src/Garage.java
@@ -1,16 +1,11 @@
 import java.util.ArrayList;
 
-import sun.util.resources.cldr.pa.CalendarData_pa_Arab_PK;
-
 public class Garage {
 	
 		private static final int maxCapacity = 10; 
 		private static Car [] array = new Car[maxCapacity]; 
 		
 		private static int nextVehicle; 
-		
-		private ArrayList <Vehicle> carParkArray; // Every new registration will of type Vehicle
-		
 		
 		//Constructor 
 		public Garage() {
@@ -21,53 +16,37 @@ public class Garage {
 			return maxCapacity; 
 		}
 	
-			public int numberOfAvailableSpots() {
-				return maxCapacity - nextVehicle;     
-						
-						
+		public int numberOfAvailableSpots() {
+				return maxCapacity - nextVehicle;     							
 			}
-			
-			
-//			public boolean isParkingEmpty() {
-//				boolean isEmpty = true;
-//				int i = 0;
-//				int placesOfParking = parking.length;
-//				while (isEmpty && i < placesOfParking) {
-//					isEmpty = (parking[i] == null);
-//					i++;
-//				}
-//				return isEmpty;
-//			}
-//
-//			public boolean isParkingFull() {
-//				boolean isFull = true;
-//				int i = 0;
-//				int placesOfParking = parking.length;
-//				while (isFull && i < placesOfParking) {
-//					isFull = !isSlotEmpty(i);
-//					i++;
-//				}
-//				return isFull;
-//			}	
-			
-			
-			
-			
+		
+		public void printOutVehicleDetails() {	
 		}
 
-		public void addVehicles(Vehicle visitor) {
-			// TODO Auto-generated method stub
+		public void addVehicles(Car visitor) {
+			if(numberOfAvailableSpots() > 0) {
+				array[nextVehicle] = visitor; 
+				nextVehicle++;
+			}	
 			
-		}
-
-		public Vehicle findPlate() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public void printOutVehicleDetails() {
-			// TODO Auto-generated method stub
-			
+			else 
+				System.out.println("Parking Full");
+		
 		}
 		
+		
+		public void removeVehicle (Car visitor) {
+			
+			for (int i = 0; i < nextVehicle; i++) {
+				if (array[i] == visitor) {
+					for (int j = i; j < nextVehicle; i--)
+						array[j] = array[j+1]; 
+				}
+			}
+			
+			}	
+		
+		public Vehicle findPlate() {
+			return null;
+		}		
 }
