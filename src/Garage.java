35c21e61f2f3bc4255b8197983a4baa256240ba7
Miguel Chourio <Byte2020>
2018-03-02 10:01:56 +0100

    Garage class

---
+++ src/Garage.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Garage {
+
+		private static final int maxCapacity = 10; 
+		private ArrayList <Vehicle> carParkArray; // Every new registration will of type Vehicle
+		
+		
+		//Constructor 
+		public Garage() {
+			this.carParkArray = new ArrayList <Vehicle>(maxCapacity);
+			
+		}
+		
+		public static int getMaxCapacity ()  {
+			return maxCapacity; 
+			
+		//Number of 
+			
+		}
+
+		public void addVehicles(Vehicle visitor) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public Vehicle findPlate() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void printOutVehicleDetails() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+}
